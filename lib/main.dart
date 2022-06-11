35e7f95da043e5e2718046699232c50c35b2cb68
Deepak-77 <deepakupreti7174@gmail.com>
2022-06-11 21:10:07 +0545

    changes

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat_app/view/auth_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(ProviderScope(child: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
+    );
+  }
+}
---
+++ lib/provider/loginProvider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final loginProvider = StateNotifierProvider<LoginProvider, bool>((ref) => LoginProvider(true));
+
+class LoginProvider extends StateNotifier<bool>{
+  LoginProvider(super.state);
+
+
+  void toggle(){
+    state = !state;
+  }
+
+}
+
+final isViewPro = ChangeNotifierProvider((ref) => IsViewProvider());
+
+class IsViewProvider extends ChangeNotifier{
+
+  bool isView = true;
+  void toggle(){
+    isView = !isView;
+    notifyListeners();
+  }
+
+}
